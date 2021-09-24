8c709b55365ba3efed9a212756deeab9172eb767
Dimario <Dimario-one@yandex.ru>
2021-09-24 19:53:44 +0600

    fixed comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,22 @@
 const popup = document.querySelector(".popup"); /*Нашел popup*/
-const popupOpenBtn = document.querySelector(".profile__editButton"); /*Нашел кнопку редактирования*/
+const popupOpenBtn = document.querySelector(".profile__edit-button"); /*Нашел кнопку редактирования*/
 const popupCloseBtn = popup.querySelector(".popup__close-button"); /*Нашел кнопку закрытия*/
 const popupForm = popup.querySelector(".popup__form"); /*Нашел форму с кнопками*/
 
 
-let formName = document.querySelector(".popup__form-name"); /*Нашел инпут имени*/
+let formName = document.querySelector(".popup__input-name"); /*Нашел инпут имени*/
 let textTitle = document.querySelector('.profile__text-title'); /*Нашел куда буду сохранять новое имя*/
 
-let formProf = document.querySelector(".popup__form-about"); /*Нашел инпут професии*/
+let formProf = document.querySelector(".popup__input-prof"); /*Нашел инпут професии*/
 let textSubtitle = document.querySelector('.profile__text-subtitle'); /*Нашел куда буду сохранять новую професию*/
 
-function popupToggle() {
-    startNameProf() /*вызвал функцию сохранения */
-    popup.classList.toggle('popup_open')
+function popupOpen() {
+    startNameProf()
+    popup.classList.add("popup_open");
+}
+
+function popupClose() {
+    popup.classList.remove("popup_open");
 }
 
 function startNameProf() {
@@ -24,9 +28,9 @@ function save(evt) {
     evt.preventDefault();
     textTitle.textContent = formName.value; /*перезаписал новое имя*/
     textSubtitle.textContent = formProf.value; /*перезаписал новую професию*/
-    popupToggle();
+    popupClose();
 }
 
-popupOpenBtn.addEventListener('click', popupToggle);
-popupCloseBtn.addEventListener('click', popupToggle);
+popupOpenBtn.addEventListener('click', popupOpen);
+popupCloseBtn.addEventListener('click', popupClose);
 popupForm.addEventListener('submit', save);
