a3fb06d8ad10132c010c34c1d76a59016b786bd5
Dimario <Dimario-one@yandex.ru>
2021-11-06 13:27:14 +0600

    sprint 7.4

--- scripts/index.js
+++ scripts/index.js
@@ -25,7 +25,7 @@ const popupImages = document.querySelector(".popup_type_image");
 const popupImage = document.querySelector(".popup_type_image"); //Нашел картинку
 const cardsUl = document.querySelector(".cards__items"); // Нашел список куда буду добавлять карточку
 const popupImageCloseBtn = popupImage.querySelector(".popup__close-image"); // Нашел кнопку закрытия попап с картинкой
-const initialCard = [{
+const initialCards = [{
         name: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     },
@@ -57,9 +57,9 @@ const cardList = document.querySelector(".cards__items");
 
 //Функции
 //
-function handleImageClick(initialCard) {
-    popupImages.querySelector(".popup__pic").src = initialCard.link;
-    popupImages.querySelector(".popup__title_white").textContent = initialCard.name;
+function handleImageClick(initialCards) {
+    popupImages.querySelector(".popup__pic").src = initialCards.link;
+    popupImages.querySelector(".popup__title_white").textContent = initialCards.name;
     openModal(popupImages);
 }
 
@@ -81,7 +81,7 @@ function newCardAdd(e) {
     closeModal(popupAddFormBtn);
 }
 
-initialCard.forEach(function(elementData) {
+initialCards.forEach(function(elementData) {
     const card = new Card(elementData, "#cards__template", handleImageClick).render();
     cardList.append(card);
 });
