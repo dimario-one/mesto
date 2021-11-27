d716ab2ee656813454065663ae678aceaa3c3e0b
Dimario <Dimario-one@yandex.ru>
2021-11-27 15:26:23 +0600

    fix validation 1

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -68,8 +68,7 @@
 
          this._element.addEventListener('submit', (evt) => { // Функция запрета  действия по умолчанию
              evt.preventDefault();
-             this._toggleButtonState(false);
-
+             this._toggleButtonState(true);
          })
      }
 
